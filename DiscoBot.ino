d71094f19786727d16ae8b592fd8f40b531d17d6
hepaestus <hepaestus@gmail.com>
2017-12-27 19:36:01 -0500

    issue with connection to AIO fixed

--- DiscoBot.ino
+++ DiscoBot.ino
@@ -22,18 +22,18 @@ Adafruit_DCMotor *R_MOTOR = AFMS.getMotor(2);
 
 #define AIO_SERVER     "io.adafruit.com"
 #define AIO_SERVERPORT 1883 
-#define AIO_USERNAME   "xxxxxxxx"
-#define AIO_KEY        "xxxxxxxxxxxxxxxxxxxxxxx"
+#define AIO_USERNAME   "xxxxx"
+#define AIO_KEY        "xxxxx"
 
 // WiFi parameters
-const char* ssid = "xxxxxxxx";
-const char* password = "xxxxxxxxx";
+const char* ssid = "xxxxx";
+const char* password = "xxxxx";
 
 // Setup the MQTT client class by passing in the WiFi client and MQTT server and login details.
 Adafruit_MQTT_Client mqtt(&client, AIO_SERVER, AIO_SERVERPORT, AIO_USERNAME, AIO_KEY);
 
 // Notice MQTT paths for AIO follow the form: <username>/feeds/<feedname>
-Adafruit_MQTT_Subscribe forward  = Adafruit_MQTT_Subscribe(&mqtt, AIO_USERNAME "/feeds/audreya-fwd");
+Adafruit_MQTT_Subscribe forward  = Adafruit_MQTT_Subscribe(&mqtt, AIO_USERNAME "/feeds/audrey.a-fwd");
 Adafruit_MQTT_Subscribe backward = Adafruit_MQTT_Subscribe(&mqtt, AIO_USERNAME "/feeds/audrey.a-bkwd");
 Adafruit_MQTT_Subscribe left     = Adafruit_MQTT_Subscribe(&mqtt, AIO_USERNAME "/feeds/audrey.a-left");
 Adafruit_MQTT_Subscribe right    = Adafruit_MQTT_Subscribe(&mqtt, AIO_USERNAME "/feeds/audrey.a-right");
@@ -57,15 +57,12 @@ void setup(void) {
 
   Serial.println("WiFi connected");
   Serial.println("IP address: "); Serial.println(WiFi.localIP());
-
-  MQTT_connect();
- 
+  
   mqtt.subscribe(&forward);
   mqtt.subscribe(&backward);
   mqtt.subscribe(&left);
   mqtt.subscribe(&right);
-  mqtt.subscribe(&stop_);
-  //mqtt.subscribe(&conn);  
+  mqtt.subscribe(&stop_); 
 
   // Init motor shield
   AFMS.begin();  
@@ -83,27 +80,27 @@ void loop() {
 
   Adafruit_MQTT_Subscribe *subscription;
   while ((subscription = mqtt.readSubscription(5000))) {
-    Serial.println(" Subscribing... ");
+    Serial.print(" Subscribing... ");
     if (subscription == &forward) {
-      Serial.print(F("Got: "));
+      Serial.print(F("Forward "));
       Serial.println((char *)forward.lastread);
       r_forward();
     } else if ( subscription == &backward ) {
-      Serial.print(F("Got: "));
+      Serial.print(F("Backward: "));
       Serial.println((char *)backward.lastread);
-
+      r_backward();
     } else if ( subscription == &left ) {
-      Serial.print(F("Got: "));
+      Serial.print(F("Left: "));
       Serial.println((char *)left.lastread);
-    
+      r_left();
     } else if ( subscription == &right ) {
-      Serial.print(F("Got: "));
+      Serial.print(F("Right: "));
       Serial.println((char *)right.lastread);
-    
+      r_right();
     } else if ( subscription == &stop_ ) {
-      Serial.print(F("Got: "));
+      Serial.print(F("Stop: "));
       Serial.println((char *)stop_.lastread);
-    
+      r_stop();
     } else {
       Serial.print((char *)subscription);
     }
@@ -114,22 +111,19 @@ void loop() {
   Serial.print(F("\nSending connection data "));
   //Serial.print();
   Serial.print("...");
-  if (! conn.publish("foobar")) {
+  if (! conn.publish(0)) {
     Serial.println(F("Failed"));
   } else {
     Serial.println(F("OK!"));
   }
-  //Serial.println("WiFi connected");
-  //Serial.println("IP address: "); Serial.println(WiFi.localIP());
   Serial.println("--- End Loop ---");
-  
 }
 
-void r_stop(AdafruitIO_Data *data) {
+void r_stop() {
   
   // Stop
   Serial.println(" --- Stop --- ");
-  
+  conn.publish("stop");
   L_MOTOR->setSpeed(0);
   L_MOTOR->run( RELEASE );
  
@@ -142,7 +136,7 @@ void r_forward() {
   
   // Stop
   Serial.println(" --- Forward --- ");
-  
+  conn.publish("forward");
   L_MOTOR->setSpeed(100);
   L_MOTOR->run( FORWARD );
  
@@ -151,10 +145,10 @@ void r_forward() {
   
 }
 
-void r_left(AdafruitIO_Data *data) {
+void r_left() {
   
   Serial.println(" --- Left --- ");
- 
+  conn.publish("left");
   L_MOTOR->setSpeed(50);
   L_MOTOR->run( BACKWARD );
  
@@ -163,11 +157,11 @@ void r_left(AdafruitIO_Data *data) {
   
 }
 
-void r_right(AdafruitIO_Data *data) {
+void r_right() {
   
   // Stop
-    Serial.println(" --- Right --- ");
-  
+  Serial.println(" --- Right --- ");
+  conn.publish("right");
   L_MOTOR->setSpeed(50);
   L_MOTOR->run( FORWARD );
  
@@ -176,10 +170,10 @@ void r_right(AdafruitIO_Data *data) {
   
 }
 
-void r_backward(AdafruitIO_Data *data) {
+void r_backward() {
   
   // Stop
-  
+  conn.publish("backward");
   L_MOTOR->setSpeed(75);
   L_MOTOR->run( BACKWARD );
  
@@ -206,7 +200,7 @@ void MQTT_connect() {
        Serial.println(mqtt.connectErrorString(ret));
        Serial.println("Retrying MQTT connection in 5 seconds...");
        mqtt.disconnect();
-       delay(8000);  // wait 5 seconds
+       delay(8000);  // wait 8 seconds
        retries--;
        if (retries == 0) {
          // basically die and wait for WDT to reset me
